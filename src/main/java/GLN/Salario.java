f8f6cb6d5dd25e0357ea82b95dd8e8d6c09557d6
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-04-23 10:49:50 -0300

    Primeiro commit

---
+++ src/main/java/GLN/Salario.java
@@ -0,0 +1,5 @@
+//2. Para calcular o salário líquido de um funcionário deve-se descontar o INSS e o 
+//imposto de renda do seu salário bruto. O desconto de INSS é de 4,5% até o limite de R$ 5000,00. 
+//Já o imposto de renda é de 12% sobre o que excede R$ 2500,00. O IR e o INSS são
+//calculados sobre o salário bruto original, sem os descontos. Explore a técnica de valor limite para identificar os casos de teste.
+
