c1d2efeabc5a4859b58d97e8cbeadf76122c53b1
LuizH12M <luiz.mosmann@acad.pucrs.br>
2019-04-23 11:26:19 -0300

    Segundo commit

---
+++ src/main/java/GLN/Funcionario.java
@@ -0,0 +1,42 @@
+//2. Para calcular o salário líquido de um funcionário deve-se descontar o INSS e o 
+//imposto de renda do seu salário bruto. O desconto de INSS é de 4,5% até o limite de R$ 5000,00. 
+//Já o imposto de renda é de 12% sobre o que excede R$ 2500,00. O IR e o INSS são
+//calculados sobre o salário bruto original, sem os descontos. Explore a técnica de valor limite para identificar os casos de teste.
+
+import java.util.*;
+
+public class Funcionario 
+{
+    private String nome;
+    private double salBruto;
+    private Strig eca;
+
+    public Funcionario (String nome, double salBruto){
+        nome = this.nome;
+        salBruto = this.salBruto;
+    }
+
+    String getNome() { return this.nome;}
+
+    double getSalarioBruto() { return this.salBruto;}
+    
+    double getSalarioLiquido() {
+        double salLiquido = salBruto - getIR() - getINSS();
+        return salLiquido;
+    }
+
+    double getIR() {
+        double ir = 0;
+        if (salBruto > 2500)
+            ir = 12 * salBruto;
+        return ir;    
+    }
+
+    double getINSS() {
+        double inss = salBruto * (4.5/100);
+        if (inss > 5000)
+            inss = 5000;
+        return inss;    
+     }
+
+}
